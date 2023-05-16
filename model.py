bed042f4a3896e9ea7b442c52a2d11d1a0e1dd09
Nathan Shwatal <nshwatal@mit.edu>
2023-05-16 13:24:20 -0400

    Add comments to model.py

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 
 gen = np.random.default_rng()
+
+# parameters
 VOWEL_LENGTH = 200
 VOWEL_VOICE_DURATION = 175
 DESIRED_DISTANCE = 80
@@ -22,7 +24,8 @@ DISTINCTIVENESS_WEIGHT = 10
 LEARNING_RATE = 0.05
 MLE_ITERATIONS = 100
 
-
+# weighted sum of constraints, with the vot, f0, vot constraint weight,
+# and f0 constraint weight as parameters
 def loss(vot, f0, w_vot, w_f0):
     return (
         w_f0 * (f0 - MALE_LEN_F0) ** 2
@@ -36,12 +39,17 @@ def loss(vot, f0, w_vot, w_f0):
     )
 
 
+# sum of constraints as a function of a vector input and weight parameters, for use
+# in minimization
 def loss_vec(x, w_vot, w_f0):
     return loss(x[0], x[1], w_vot, w_f0)
 
+# formula for a gaussian
 def gaussian(x, mean, std):
         return 1/(std*np.sqrt(2*np.pi)) * np.exp((-1/2)*((x-mean)/std)**2)
 
+# given an initial guess for vot and f0, perform the minimization and
+# produce a likelihood function based on the values
 def make_likelihood_func(vot_0, f0_0):
 
     def likelihood_func(vot, f0, w_vot, w_f0):
@@ -61,6 +69,8 @@ def make_likelihood_func(vot_0, f0_0):
 
     return likelihood_func
 
+# take in a likelihood function and weight values to produce an approximation of
+# the derivative with respect to vot using a limit approximation
 def make_w_vot_deriv(likelihood_func, w_vot, w_f0):
 
     def func(vot, f0):
@@ -68,6 +78,8 @@ def make_w_vot_deriv(likelihood_func, w_vot, w_f0):
 
     return func
 
+# take in a likelihood function and weight values to produce an approximation of
+# the derivative with respect to f0 using a limit approximation
 def make_w_f0_deriv(likelihood_func, w_vot, w_f0):
 
     def func(vot, f0):
@@ -81,10 +93,12 @@ w_vots = [1.0]
 w_f0s = [5.0]
 
 
-
+# initial weights
 weights = np.asarray([1.0, 5.0])
+# initial values of vot and f0
 x0 = [120, 4.54]
 for epoch in range(NUM_EPOCHS):
+    # find optimal values of vot and f0
     res = minimize(
         loss_vec,
         x0,
@@ -96,45 +110,54 @@ for epoch in range(NUM_EPOCHS):
     current_vot, current_f0 = res
     vots.append(current_vot)
     f0s.append(current_f0)
-    print(f"{current_vot= }, {current_f0= }")
+    # print(f"{current_vot= }, {current_f0= }")
     current_w_vot, current_w_f0 = weights
 
     likelihood_func = make_likelihood_func(current_vot, current_f0)
 
+    # normally distribute around the current values
+    vot_list = gen.normal(current_vot, VOT_STD, TRIALS_PER_EPOCH)
+    f0_list = gen.normal(current_f0, F0_STD, TRIALS_PER_EPOCH)
+
+    # bias the VOT
+    biased_vot = vot_list + VOT_BIAS
+
+    # MLE gradient ascent
     for iteration in range(MLE_ITERATIONS):
+        # calculate the derivatives
         vot_deriv = make_w_vot_deriv(likelihood_func, current_w_vot, current_w_f0)
         f0_deriv = make_w_f0_deriv(likelihood_func, current_w_f0, current_w_f0)
-
-        vot_list = gen.normal(current_vot, VOT_STD, TRIALS_PER_EPOCH)
-        f0_list = gen.normal(current_f0, F0_STD, TRIALS_PER_EPOCH)
-        biased_vot = vot_list + VOT_BIAS
 
         vot_update_list = vot_deriv(biased_vot, f0_list)
         f0_update_list = f0_deriv(biased_vot, f0_list)
 
+        # compute mean over all datapoints
         vot_update_mean = np.mean(vot_update_list)
         f0_update_mean = np.mean(f0_update_list)
 
         current_w_vot += LEARNING_RATE * vot_update_mean
         current_w_f0 += LEARNING_RATE * f0_update_mean
 
-    print(current_w_vot, current_w_f0)
+    # print(current_w_vot, current_w_f0)
     w_vots.append(current_w_vot)
     w_f0s.append(current_w_f0)
+
+    # update the weights and strart again
     weights = [current_w_vot, current_w_f0]
 
-vots_d = pd.DataFrame(vots)
-vots_d.to_excel("vot_data.xlsx")
-f0s_d = pd.DataFrame(f0s)
-f0s_d.to_excel("f0_data.xlsx")
-plt.plot(vots)
-plt.show()
-plt.plot(f0s)
-plt.show()
-plt.plot(w_vots)
-plt.show()
-plt.plot(f0s)
-plt.show()
+# plotting and saving data to spreadsheets
+# vots_d = pd.DataFrame(vots)
+# vots_d.to_excel("vot_data.xlsx")
+# f0s_d = pd.DataFrame(f0s)
+# f0s_d.to_excel("f0_data.xlsx")
+# plt.plot(vots)
+# plt.show()
+# plt.plot(f0s)
+# plt.show()
+# plt.plot(w_vots)
+# plt.show()
+# plt.plot(f0s)
+# plt.show()
 
 
 # x0 = np.array([105, 5])
